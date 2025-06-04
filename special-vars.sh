16cba2ec59deb075822dd89cef2fc9eccd38f399
sameer786 <sameeralishaik7867@gmail.com>
2025-06-04 17:54:50 +0530

    special vars

--- special-vars.sh
+++ special-vars.sh
@@ -2,11 +2,11 @@
 
 
 echo "All variables passed to the script: $@"
-number of variables passed: $#
-script name: $0
-present working directory: $PWD
-home directory of the user who is running the script: $HOME
-which user is running the script: $USER
-PID of the current script: $$
+echo "number of variables passed: $#"
+echo "script name: $0"
+echo "present working directory: $PWD"
+echo "directory of the user who is running the script: $HOME"
+echo "which user is running the script: $USER"
+echo "PID of the current script: $$"
 sleep 10 &
-PID of the last command running in background: $!
+echo "PID of the last command running in background: $!"
